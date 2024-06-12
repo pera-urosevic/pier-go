60953d4bd2a25d71e98a057a8142a12df1948a67
Pera Urosevic <pera.urosevic@gmail.com>
2024-06-12 20:15:59 +0300

    remove print

--- reader/storage/cleanup.go
+++ reader/storage/cleanup.go
@@ -1,7 +1,6 @@
 package storage
 
 import (
-	"fmt"
 	"pier/database"
 	"pier/notify"
 	"time"
@@ -10,7 +9,6 @@ import (
 func Cleanup() {
 	db := database.Connect()
 	threshold := time.Now().Add(-1 * time.Hour * 24 * 28).Format("2006-01-02")
-	fmt.Println("threshold", threshold)
 	_, err := db.Exec("DELETE FROM `reader_articles` WHERE `discarded` = 1 AND `id` < ?", threshold)
 	if err != nil {
 		notify.Warn("reader", "Cleanup: "+err.Error())
