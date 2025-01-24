a0d96dab53ba8e0d9b0b1c048d71523dcf4be7b2
Pera Urosevic <pera.urosevic@gmail.com>
2025-01-24 13:18:48 +0200

    gorm pool

--- storage/storage.go
+++ storage/storage.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"os"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -13,7 +14,21 @@ func DB() (*gorm.DB, error) {
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	name := os.Getenv("DB_NAME")
+
 	dsn := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + name + "?parseTime=True"
 	db, err := gorm.Open(mysql.New(mysql.Config{DSN: dsn}), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB.SetMaxIdleConns(2)
+	sqlDB.SetMaxOpenConns(6)
+	sqlDB.SetConnMaxLifetime(time.Minute * 1)
+
 	return db, err
 }
